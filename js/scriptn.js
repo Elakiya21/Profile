293ed08ab817b84239f7882c3e4037f9b83e3a88
Elakiya21 <elakiyabaskaran21@gmail.com>
2020-06-14 23:21:53 +0530

    my profile

---
+++ js/scriptn.js
@@ -0,0 +1,13 @@
+$('.counter-count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 30000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+
